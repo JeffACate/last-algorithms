85d3c31a70894347cbda6f592bbf18983e01dfa6
Jeff Cate <58610555+JeffACate@users.noreply.github.com>
2026-03-09 21:52:39 -0500

    implement and test grow

--- src/__tests__/ArrayList.ts
+++ src/__tests__/ArrayList.ts
@@ -1,7 +1,16 @@
 import ArrayList from "@code/ArrayList";
 import { test_list } from "./ListTest";
 
-test("array-list", function () {
+// test("array-list", function () {
+//     const list = new ArrayList<number>(3);
+//     test_list(list);
+// });
+
+test('grow', function () {
     const list = new ArrayList<number>(3);
-    test_list(list);
+    expect(list.capacity).toEqual(3);
+    list.grow();
+    expect(list.capacity).toEqual(6);
+    list.grow();
+    expect(list.capacity).toEqual(12);
 });
--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,42 @@
 export default class ArrayList<T> {
     public length: number;
+    public capacity: number;
+    private items: T[];
 
-    
-
-    constructor() {
+    constructor(capacity: number) {
+        this.length = 0;
+        this.capacity = capacity;
+        this.items = new Array<T>(capacity);
     }
 
     prepend(item: T): void {
+        if (this.length === 0){
+            this.items[0] = item;
+        }
 
-}
+        this.length++;
+    }
     insertAt(item: T, idx: number): void {
 
-}
+    }
     append(item: T): void {
 
-}
-    remove(item: T): T | undefined {
+    }
+    // remove(item: T): T | undefined {
 
-}
+    // }
     get(idx: number): T | undefined {
+        return this.items[idx];
+    }
+    // removeAt(idx: number): T | undefined {
 
-}
-    removeAt(idx: number): T | undefined {
-
-}
+    // }
+    grow() {
+        this.capacity *= 2;
+        const newItems = new Array<T>(this.capacity);
+        for (let i = 0; i < this.length; i++) {
+            newItems[i] = this.items[i];
+        }
+        this.items = newItems;
+    }
 }
