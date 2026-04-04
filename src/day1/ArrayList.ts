5fee12c5ce39d8d687d2e8897beb7154d7aaff27
Jeff Cate <58610555+JeffACate@users.noreply.github.com>
2026-04-03 22:03:40 -0500

    Complete ArrayList

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -10,27 +10,53 @@ export default class ArrayList<T> {
     }
 
     prepend(item: T): void {
-        if (this.length === 0){
-            this.items[0] = item;
-        }
-
-        this.length++;
+        this.insertAt(item, 0);
     }
     insertAt(item: T, idx: number): void {
+        if (this.length === this.capacity){
+            this.grow();
+        }
 
+        var next = item;
+        
+        for (let index = idx; index < this.items.length; index++) {
+            const temp = this.items[index];
+            this.items[index] = next;
+            next = temp;
+        }
+        this.length++;
     }
     append(item: T): void {
-
+        if (this.length === this.capacity) {
+            this.grow();
+        }
+        this.items[this.length] = item;
+        this.length++;
     }
-    // remove(item: T): T | undefined {
-
-    // }
+    remove(item: T): T | undefined {
+        for (let index = 0; index < this.items.length; index++) {
+            if (this.items[index] === item) {
+                return this.removeAt(index);
+            }
+        }
+        return undefined;
+    }
     get(idx: number): T | undefined {
         return this.items[idx];
     }
-    // removeAt(idx: number): T | undefined {
+    removeAt(idx: number): T | undefined {
+        if (idx > this.length){
+            return undefined;
+        }
+        
+        const item = this.items[idx];
 
-    // }
+        for (let index = idx; index < this.items.length; index++) {
+            this.items[index] = this.items[index + 1];
+        }
+        this.length--;
+        return item;
+    }
     grow() {
         this.capacity *= 2;
         const newItems = new Array<T>(this.capacity);
