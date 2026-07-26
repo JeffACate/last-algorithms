4f303abc3e714a07af5f896f2940c38f5c0f83f0
Jeff Cate <Jeff.A.Cate@gmail.com>
2026-07-25 20:43:58 -0500

    implement quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,36 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, low, high);
+
+    qs(arr, low, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, high);
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    
+    const pivot = arr[high];
+
+    let index = low - 1;
+
+    for (let i = low; i < high; ++i) {
+        if (arr[i] <= pivot) {
+            index++;
+            const temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp; 
+        }
+    }
+
+    index++;
+    arr[high] = arr[index];
+    arr[index] = pivot;
+
+    return index;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1) 
 }
