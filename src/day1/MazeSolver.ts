dbf32682b199e676f755915b40b12fffa0586769
Jeff Cate <Jeff.A.Cate@gmail.com>
2026-07-25 20:34:21 -0500

    implement maze solver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -6,15 +6,12 @@ const dir = [
 ]
 
 function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]) : boolean {
-    
-
 
     if (curr.x < 0 || curr.x >= maze[0].length ||
         curr.y < 0 || curr.y >= maze.length) {
         
         return false;
     }
-
 
     if (maze[curr.y][curr.x] === wall) {
         return false;
